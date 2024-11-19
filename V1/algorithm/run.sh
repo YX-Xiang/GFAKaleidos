2f59ad41694fe9ef6920fb794607d919b7ef1106
YX-Xiang <997214586@qq.com>
2024-11-19 03:34:25 +0000

    2024/11/19

---
+++ V1/algorithm/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INPUT_GFA=$1
+FILENAME=$(basename "$INPUT_GFA" | sed 's/\.[^.]*$//')
+
+mkdir -p "data/$FILENAME/data"
+OUTPUT_PATH="data/$FILENAME/data"
+
+make  -C ./algorithm clean > build.log 2>&1
+make  -C ./algorithm -j >> build.log 2>&1
+# make -C ./algorithm clean
+# make -C ./algorithm -j
+
+./algorithm/gfaKaleidos.exe ${INPUT_GFA} -o ${OUTPUT_PATH} -t 16
